9523d65a8c55aa299207463cfa2c8390fce8f931
Huynh Van Tai <taiprogramer@protonmail.com>
2022-04-07 21:39:55 +0700

    [app: sign up page: add form]

--- app_frontend/simple_poker_game/lib/main.dart
+++ app_frontend/simple_poker_game/lib/main.dart
@@ -69,6 +69,66 @@ class SignInPage extends StatelessWidget {
   }
 }
 
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  SignUpFormState createState() {
+    return SignUpFormState();
+  }
+}
+
+class SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Sign up',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 10.0),
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username'),
+              )),
+          Container(
+              margin: const EdgeInsets.only(top: 10.0),
+              child: TextFormField(
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your password'),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {}
+              },
+              child: const Text('Sign up'))
+        ],
+      ),
+    );
+  }
+}
+
 class SignUpPage extends StatelessWidget {
   static const String routeName = '/signUp';
   const SignUpPage({Key? key, this.title = 'Simple Poker Game'})
@@ -80,6 +140,7 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(title)),
-        body: const Center(child: Text('Sign up page works!')));
+        body: Container(
+            padding: const EdgeInsets.all(20.0), child: const SignUpForm()));
   }
 }
