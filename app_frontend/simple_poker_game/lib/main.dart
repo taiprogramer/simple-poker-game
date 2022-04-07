fca8ffcd1278131b5e51e7a2a47949f892a18671
Huynh Van Tai <taiprogramer@protonmail.com>
2022-04-07 20:31:28 +0700

    [app: add navigation to sign up / sign in page]

--- app_frontend/simple_poker_game/lib/main.dart
+++ app_frontend/simple_poker_game/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Center(child: Text("Hello, Flutter")));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,11 +9,77 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var routes = <String, WidgetBuilder>{
+      SignInPage.routeName: (BuildContext context) => const SignInPage(),
+      SignUpPage.routeName: (BuildContext context) => const SignUpPage(),
+    };
+    return MaterialApp(
+      title: 'Simple Poker Game',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const HomePage(),
+      routes: routes,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Simple Poker Game')),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, SignUpPage.routeName);
+                },
+                child: const Text('Sign up')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, SignInPage.routeName);
+                },
+                child: const Text('Sign in')),
+          ],
+        )));
+  }
+}
+
+class SignInPage extends StatelessWidget {
+  static const String routeName = '/signIn';
+
+  const SignInPage({Key? key, this.title = 'Simple Poker Game'})
+      : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Simple Poker Game'),
+        title: Text(title),
       ),
-      body: const Center(child: Text("Hello, Flutter")),
+      body: const Center(child: Text('Login Page works!')),
     );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  static const String routeName = '/signUp';
+  const SignUpPage({Key? key, this.title = 'Simple Poker Game'})
+      : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: const Center(child: Text('Sign up page works!')));
   }
 }
